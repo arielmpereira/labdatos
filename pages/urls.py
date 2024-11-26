5ed4a3f19a1c577c33115e78493d65f6fc62935b
Ariel Pereira <arielmpereira@gmail.com>
2024-11-26 14:52:03 -0300

    update

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('acerca-de/', views.acerca_de, name='acerca_de'),
-    path('contacto/', views.contacto, name='contacto'),
-]
--- pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def acerca_de(request):
-    return render(request, 'pages/acerca_de.html')
-
-def contacto(request):
-    return render(request, 'pages/contacto.html')
